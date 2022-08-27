543cccf92ec0035eacf920878b2e0352bec1c03f
Евгений <redcheeken@yandex.ru>
2022-08-27 19:41:25 +0300

    Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// Случайное число:
+// Random rand = new Random();
+// int x = rand.Next (100, 1000);
+// Console.WriteLine(x);
+
+// Ручной ввод трехзначного числа:
+int x;
+Console.WriteLine("Введите  трехзначное число: ");
+x = Convert.ToInt32(Console.ReadLine());
+int a1 = x / 10;
+int a2 = a1 % 10;
+Console.Write(a2);
